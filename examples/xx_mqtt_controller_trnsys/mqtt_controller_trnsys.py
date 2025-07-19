dc5f9367271e6416c25dc3017564a601f320f0d0
Maximilian Beyer <maximilian.beyer@tu-dresden.de>
2025-07-19 22:09:41 +0200

    fully double checked config, added better in- and output handling, update_heat_demand-function added

--- examples/xx_mqtt_controller_trnsys/mqtt_controller_trnsys.py
+++ examples/xx_mqtt_controller_trnsys/mqtt_controller_trnsys.py
@@ -4,7 +4,7 @@ Description: This module contains the definition of a example service \
 Author: Maximilian Beyer
 """
 
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import Optional, Union
 
 from loguru import logger
@@ -38,10 +38,13 @@ class MQTTControllerTrnsys(ControllerBasicService):
             *args: Variable length argument list
             **kwargs: Arbitrary keyword arguments
         """
-        self.controller_config: Optional[ControllerComponentModel] = None
+        self.controller_component: Optional[ControllerComponentModel] = None
+        self.service_inputs: Optional[dict] = None
+        self.service_outputs: Optional[dict] = None
         self.controller_outputs_for_trnsys: Optional[OutputModel] = None
         self.data: Optional[InputDataModel] = None
         self.timestamp_last_output: datetime = datetime.now()
+        self.heat_demand: bool = False
         super().__init__(*args, **kwargs)
 
     def prepare_start(self) -> None:
@@ -52,7 +55,7 @@ class MQTTControllerTrnsys(ControllerBasicService):
         logger.info("Prepare Start of Service")
 
         # using basic service methods to get the controller configuration
-        self.controller_config = next(
+        self.controller_component = next(
             (
                 c
                 for c in self.config.controller_components
@@ -60,6 +63,14 @@ class MQTTControllerTrnsys(ControllerBasicService):
             ),
             None,
         )
+
+        # get a dict with the outputs of the controller to hold the values
+        if self.controller_component is not None and self.controller_component.outputs is not None:
+            self.service_outputs = {key: 0 for key in self.controller_component.outputs.keys()}
+        else:
+            self.service_outputs = {}
+
+        # TODO MB: may be obsolete, check if outputs for trnsys are seperately needed
         self.controller_outputs_for_trnsys = self._get_output_entity_config(
             output_entity_id="TRNSYS-Inputs"
         )
@@ -84,41 +95,6 @@ class MQTTControllerTrnsys(ControllerBasicService):
                     return entity
         raise ValueError(f"Input entity with ID '{entity_id}' not found in data")
 
-    def check_heat_demand(
-        self,
-        temperature_setpoint: float,
-        temperature_measured: float,
-        hysteresis: float,
-        demand_status_old: int,
-    ) -> int:
-        """
-        TODO MB: update docstring
-        Function to check if the heat demand should be on or off \
-            based on a 2 point controller with hysteresis
-
-        Args:
-            temperature_setpoint (float): The setpoint temperature
-            temperature_measured (float): The measured temperature (acutal temperature)
-            hysteresis (float): The hysteresis of the controller
-            heater_status_old (bool): The old status of the heater
-
-        Returns:
-            bool: The new status of the heater
-        """
-        if temperature_measured < temperature_setpoint - hysteresis:
-            return 1
-
-        if temperature_measured > temperature_setpoint:
-            return 0
-
-        if (
-            demand_status_old
-            and temperature_measured > temperature_setpoint - hysteresis
-        ):
-            return 1
-
-        return 0
-
     def entity_fully_updated_after_last_output(
         self, input_entity: InputDataEntityModel
     ) -> bool:
@@ -133,7 +109,7 @@ class MQTTControllerTrnsys(ControllerBasicService):
                 return False
         return True
 
-    def get_controller_inputs(self, input_entities: list[InputDataEntityModel]) -> dict:
+    def get_current_controller_inputs(self, input_entities: list[InputDataEntityModel]) -> dict:
         """
         Function to get the inputs for the controller from the input entities
         Args:
@@ -141,11 +117,11 @@ class MQTTControllerTrnsys(ControllerBasicService):
         Returns:
             dict: Dictionary with the inputs in the format {input_key: value}
         """
-        if self.controller_config is None:
+        if self.controller_component is None:
             raise ValueError("Prepare the start of the service before calculation")
-        
+
         inputs = {}
-        for input_key, input_config in self.controller_config.inputs.items():
+        for input_key, input_config in self.controller_component.inputs.items():
             inputs[input_key] = self.get_input_value(
                 input_entities=input_entities, input_config=input_config
             )
@@ -180,8 +156,49 @@ class MQTTControllerTrnsys(ControllerBasicService):
                             )
                             data = None
                         return data
-                    
+
         raise ValueError(f"Input data {input_config['entity']} not found")
+
+    def update_heat_demand(
+        self, t_bot: float, t_top: float, t_set: float, on_hys: int, off_hys: int
+    ) -> None:
+        """
+        Function to check if a heat demand is needed based on 2 sensors in heat storage
+        Args:
+            t_bot (float): Temperature of the bottom sensor
+            t_top (float): Temperature of the top sensor
+            t_set (float): Set point temperature
+            on_hys (int): Switch-on hysteresis
+            off_hys (int): Switch-off hysteresis
+        Returns:
+            None: set self.heat_demand to True if heat demand is needed, False otherwise
+        """
+        if self.controller_component is None:
+            raise ValueError("Prepare the start of the service before calculation")
+
+        # save old demand
+        heat_demand_old = self.heat_demand
+
+        # check whether to switch on
+        if (
+            heat_demand_old == 0
+            and t_top < t_set + on_hys
+        ):
+            self.heat_demand = True
+
+        # check whether to switch off
+        if (
+            heat_demand_old == 1
+            and t_bot > t_set + off_hys
+        ):
+            self.heat_demand = False
+
+        # if demand changed, log it
+        if heat_demand_old != self.heat_demand:
+            if self.heat_demand:
+                logger.info("Heat demand switched ON")
+            else:
+                logger.info("Heat demand switched OFF")
 
     async def calculation(self, data: InputDataModel) -> Union[DataTransferModel, None]:
         """
@@ -190,7 +207,7 @@ class MQTTControllerTrnsys(ControllerBasicService):
             data (InputDataModel): Input data with the measured values for the calculation
         """
         # check if the controller configuration is available
-        if self.controller_config is None or self.controller_outputs_for_trnsys is None:
+        if self.controller_component is None or self.controller_outputs_for_trnsys is None:
             raise ValueError("Prepare the start of the service before calculation")
 
         # using basic service methods to get the input entities
@@ -212,52 +229,74 @@ class MQTTControllerTrnsys(ControllerBasicService):
         # get the inputs needed by the controller from the input entities
         input_entities = [trnsys_input_entity, boiler_input_entity]
 
-        inputs = self.get_controller_inputs(input_entities)
+        self.service_inputs = self.get_current_controller_inputs(input_entities)
+
+        self.update_heat_demand(
+            t_bot=self.service_inputs["t_heat_bot"],
+            t_top=self.service_inputs["t_heat_top"],
+            t_set=self.service_inputs["t_heat_set"],
+            on_hys=self.controller_component.config["hysteresis_heat_on"],
+            off_hys=self.controller_component.config["hysteresis_heat_off"]
+        )
 
         components = []
         sammeln_payload = ""
 
         # TODO MB: Calculate the values based on the inputs here, add them to the DataTransferComponentModel and the sammeln_payload, if for TRNSYS-Inputs
 
-        for output_key, output_config in self.controller_config.outputs.items():
+        for output_key, output_config in self.controller_component.outputs.items():
+            # skip the full_trnsys_message, it is handled separately
             if output_key == "full_trnsys_message":
-                # skip the full output, it is handled separately
                 continue
 
             entity_id = output_config["entity"]
             attribute_id = output_config["attribute"]
 
-            # add standard message of the outputs to DataTransferComponentModel
-            components.append(
-                DataTransferComponentModel(
-                    entity_id=entity_id,
-                    attribute_id=attribute_id,
-                    value=None,
-                    timestamp=datetime.now(timezone.utc),
-                )
-            )
+            # check if the output is needed for TRNSYS
+            if entity_id == "TRNSYS-Inputs":
+                # add it to the full_trnsys_message
+                try:
+                    value = self.service_outputs[output_key] if self.service_outputs is not None else 0
+                except KeyError:
+                    # TODO MB: this should not happen, delete this try-except after debugging
+                    logger.error(
+                        f"Output key '{output_key}' not found in service outputs, set it to 0."
+                        " This may indicate a initialization configuration issue."
+                    )
+                    value = 0
+                sammeln_payload += f"{attribute_id} : {value} # "
 
-            # build the trnsys payload for the full message
-            for output_attribute in self.controller_outputs_for_trnsys.attributes:
-                if output_attribute.id == attribute_id:
-                    if output_attribute.id == "OP_S_WP":
-                        trnsys_value = 1
-                    elif output_attribute.id == "OP_S_WP_TWE":
-                        trnsys_value = 0
-                    elif output_attribute.id == "n_WP":
-                        trnsys_value = 0.25
-                    elif output_attribute.id == "S_PK":
-                        trnsys_value = 0
-                    elif output_attribute.id == "S_PK_TWE":
-                        trnsys_value = 0
-                    elif output_attribute.id == "m_PK":
-                        trnsys_value = 0
-                    elif output_attribute.id == "tA_PK":
-                        trnsys_value = 50
-                    else:
-                        trnsys_value = 0
-                    trnsys_variable_name = output_attribute.id_interface
-                    sammeln_payload += f"{trnsys_variable_name} : {trnsys_value} # "
+        #     # add standard message of the outputs to DataTransferComponentModel
+        #     components.append(
+        #         DataTransferComponentModel(
+        #             entity_id=entity_id,
+        #             attribute_id=attribute_id,
+        #             value=None,
+        #             timestamp=datetime.now(timezone.utc),
+        #         )
+        #     )
+
+        #     # build the trnsys payload for the full message
+        #     for output_attribute in self.controller_outputs_for_trnsys.attributes:
+        #         if output_attribute.id == attribute_id:
+        #             if output_attribute.id == "OP_S_WP":
+        #                 trnsys_value = 1
+        #             elif output_attribute.id == "OP_S_WP_TWE":
+        #                 trnsys_value = 0
+        #             elif output_attribute.id == "n_WP":
+        #                 trnsys_value = 0.25
+        #             elif output_attribute.id == "S_PK":
+        #                 trnsys_value = 0
+        #             elif output_attribute.id == "S_PK_TWE":
+        #                 trnsys_value = 0
+        #             elif output_attribute.id == "m_PK":
+        #                 trnsys_value = 0
+        #             elif output_attribute.id == "tA_PK":
+        #                 trnsys_value = 50
+        #             else:
+        #                 trnsys_value = 0
+        #             trnsys_variable_name = output_attribute.id_interface
+        #             sammeln_payload += f"{trnsys_variable_name} : {trnsys_value} # "
 
         # add trnsys full message to DataTransferComponentModel
         components.append(
@@ -265,9 +304,11 @@ class MQTTControllerTrnsys(ControllerBasicService):
                 entity_id="TRNSYS-Inputs",
                 attribute_id="trnsys_sammeln",
                 value=sammeln_payload,
-                timestamp=datetime.now(timezone.utc),
+                timestamp=datetime.now(),
             )
         )
+
+
 
         # set the current time as the new timestamp of the last output
         self.timestamp_last_output = datetime.now()
